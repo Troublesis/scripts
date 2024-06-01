fe2df8dfe632746f0ae7f92954461d4b96468eca
Troublesis <40723560+Troublesis@users.noreply.github.com>
2024-06-01 14:20:59 +1000

    Update docker-compose-init.sh

--- bash/docker-compose-init.sh
+++ bash/docker-compose-init.sh
@@ -5,6 +5,7 @@ cd /docker || { echo "Failed to change directory to /docker"; exit 1; }
 
 # Create a root docker project folder
 read -p "Enter root folder name for docker project: " -a root
+sudo mkdir /docker/$root
 cd "./$root"
 
 # Create a docker-compose.yml file in the folder
